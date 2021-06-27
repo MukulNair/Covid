e3e4d288279eed38f4ffe6e6315bb510c15a6dce
sai ram kaushik <sairamkaushik1@gmail.com>
2021-06-27 22:07:01 +0530

    added contact.php to the main project file

---
+++ mukul/contact.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="C.css">
+</head>
+<body style="background-color:powderblue;">
+    <div class="container-fluid">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+      
+      
+        <nav id="grey"> 
+            <center>
+                <a class="simp" href="contact.html"><b>Home</b> </a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href="Why.html"> <b>Why We're Here</b> </a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href="Team.html"> <b>Team</b>  </a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+                <a href="Contact.html"> <b>Contact Us</b> </a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </center>  
+      </nav>
+      <img src="contact.jpg" style="width:1500px;height:500px;">  
+        <hr class="hr">
+      
+      <div class="container">
+
+          <p><h1><b><u>About Us</u></b></h1>
+
+        <p> <h1>Manav Rachna International Institute of Research and Studies</h4>
+            <ul> 
+              <li>Manav Rachna Campus Rd</li>
+              <li>Gadakhor Basti Village, Sector 43</li>
+              <li>Faridabad, Haryana 121004</li>
+              <li>Near Aravalli Hills</li>
+              
+            </ul>
+              
+        <form action="mailto:m3mj2002@gmail.com"   method= "POST" 
+        enctype=”multipart/form-data”
+         name=”EmailForm”>
+         
+         <p> Name </p> <br>  <input type="text" name="name">
+         
+         <p> Email </p> <br>   <input type="email" name="_replyto">
+         <br>
+         <p> QUERIES</p> <br>  <input type="text" name="query">
+                <input type="submit" value="Send">
+                <br><br><br><br>
+      </form>
+        
+        
+          </div>
+        </div>
+</body>
+</html>
